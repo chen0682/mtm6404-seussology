135f7b31408c28b6f3ee37c50e6ed06a879845b2
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-01-20 05:32:02 -0500

    Adds search and sorting

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const app = new Vue({
   el: '#app',
   data: {
     title: 'Seussology',
-    books: books,
-    category: ''
+    category: '',
+    query: '',
+    sortBy: 'title',
+    selectedBook: false
+  },
+  computed: {
+    books: function () {
+      if (this.query) {
+        return books.filter((book) =>
+          book.title.toLowerCase().includes(this.query.toLowerCase())
+        ).sort((a, b) => a[this.sortBy] - b[this.sortBy])
+      }
+
+      return books.slice(0).sort((a, b) => a[this.sortBy] - b[this.sortBy])
+    }
   }
 })
