e8fb1fa51770acd170cafa939808ba046631680f
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-01-20 13:54:29 -0500

    Changes for week 2

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: books,
+    category: ''
+  }
+})
+
+// if (category) {
+//   book.category === category
+// } else {
+//   true
+// }
+
+// (category) ? book.category === category  : true
