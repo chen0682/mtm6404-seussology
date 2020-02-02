dff018dbb07112d9209688a3bc027e6216ebd604
chen0682 <chen0682@algonquinlive.com>
2020-02-01 20:48:51 -0500

    week-3-ends

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ const app = new Vue({
   el: '#app',
   data: {
     title: 'Seussology',
-    books: books,
-    category: ''
+    // books: books,
+    category: '',
+    query: '',
+    sortBy: 'title'
+  },
+  computed: {
+    books: function () {
+      if (this.query) {
+        /* const self = this
+        return books.filter(function (book) {
+          book.tittle.includes(self.query)// this inside the anonymous function refers to the window.
+        }) */
+        return books.filter(book => book.title.toLowerCase().includes(this.query.toLowerCase())).sort((a, b) => a[this.sortBy] - b[this.sortBy])
+      }
+      return books.slice(0).sort((a, b) => a[this.sortBy] - b[this.sortBy])
+    }
   }
+
 })
